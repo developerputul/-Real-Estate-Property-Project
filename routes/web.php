59f420bc6b651a43c969c1b66521b045f7ca7494
developerputul <putulsentongchangya7@gmail.com>
2024-04-24 23:53:28 +0600

    Frontend User Profile Setting Section Updated

--- routes/web.php
+++ routes/web.php
@@ -31,13 +31,17 @@ Route::get('/dashboard', function () {
     return view('dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
 
-Route::middleware('auth')->group(function () {
-    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
-    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
-    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
-});
 
+
+Route::middleware('auth')->group(function () {
+    Route::get('/user/profile', [UserController::class, 'UserProfile'])->name('user.profile');
+    Route::post('/user/profile/store', [UserController::class, 'UserProfileStore'])->name('user.profile.store');
+  
+});
 require __DIR__.'/auth.php';
+
+
+
 
 
 //Admin Group Middleware
