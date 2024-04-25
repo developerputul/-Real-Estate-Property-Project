8fbf34631b519df3eec83b5e204154b3a8ac9172
developerputul <putulsentongchangya7@gmail.com>
2024-04-25 14:22:02 +0600

    Admin Dashboard Property Type Section Updated

---
+++ app/Http/Controllers/Backend/PropertyTypeController.php
@@ -0,0 +1,80 @@
+<?php
+
+namespace App\Http\Controllers\Backend;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use App\Models\PropertyType;
+
+class PropertyTypeController extends Controller
+{
+    public function AllType(){
+
+        $types = PropertyType::latest()->get();
+        return view('backend.type.all_type', compact('types'));
+    } // End Method
+
+    public function AddType(){
+        return view('backend.type.add_type');
+
+    } // End Method
+
+    public function StoreType(Request $request){
+         //validation
+         $request->validate([
+            'type_name' => 'required|unique:property_types|max:200',
+            'type_icon' => 'required'
+        ]);
+
+        PropertyType::insert([
+            'type_name' => $request->type_name,
+            'type_icon' => $request->type_icon,
+
+        ]);
+        $notification = array(
+            'message' => 'Property Type Create Successfully',
+            'alert-type' => 'success'
+        );
+
+        return redirect()->route('all.type')->with($notification);
+
+    } // End Method
+
+    public function EditType($id){
+        $types = PropertyType::findOrFail($id);
+        return view('backend.type.edit_type', compact('types'));
+
+    } //End Method
+
+    public function UpdateType(Request $request){
+    
+        $pid = $request->id;
+
+       PropertyType::findOrFail($pid)->update([
+           'type_name' => $request->type_name,
+           'type_icon' => $request->type_icon,
+
+       ]);
+       $notification = array(
+           'message' => 'Property Type Updated Successfully',
+           'alert-type' => 'success'
+       );
+
+       return redirect()->route('all.type')->with($notification);
+
+   } // End Method
+
+   public function DeleteType($id){
+
+    PropertyType::findOrFail($id)->delete();
+  $notification = array(
+           'message' => 'Property Type Deleted Successfully',
+           'alert-type' => 'success'
+       );
+
+       return redirect()->back()->with($notification);
+
+   } // End Method
+
+}
--- routes/web.php
+++ routes/web.php
@@ -5,7 +5,7 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AdminController;
 use App\Http\Controllers\AgentController;
 use App\Http\Controllers\UserController;
-
+use App\Http\Controllers\Backend\PropertyTypeController;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -78,6 +78,26 @@ Route::get('/agent/dashboard', [AgentController::class, 'AgentDashboard'])->name
 
 }); //End Group Agent Middleware
 
-
-
 Route::get('/admin/login', [AdminController::class, 'AdminLogin'])->name('admin.login');
+
+
+//Admin Group Middleware
+Route::middleware(['auth','role:admin'])->group(function(){
+ 
+//Property Type All Route
+Route::controller(PropertyTypeController::class)->group(function(){
+
+    Route::get('/all/type', 'AllType')->name('all.type');
+    Route::get('/add/type', 'AddType')->name('add.type');
+
+    Route::post('/store/type', 'StoreType')->name('store.type');
+    Route::get('/edit/type{id}', 'EditType')->name('edit.type');
+
+    Route::post('/update/type', 'UpdateType')->name('update.type');
+    Route::get('/delete/type{id}', 'DeleteType')->name('delete.type');
+
+
+
+});
+
+}); //End Group Admin Middleware
