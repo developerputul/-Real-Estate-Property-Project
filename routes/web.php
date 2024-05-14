d67fca1ffb57e3bfa0c4fcbfa1d2bdc2fd00b4e4
developerputul <putulsentongchangya7@gmail.com>
2024-05-14 14:04:05 +0600

    Backend Admin Import Section Updated

--- routes/web.php
+++ routes/web.php
@@ -306,6 +306,10 @@ Route::controller(SettingController::class)->group(function(){
 
         Route::post('/update/permission', 'UpdatePermission')->name('update.permission');
         Route::get('/delete/permission{id}', 'DeletePermission')->name('delete.permission');
+
+
+        Route::get('/import/permission', 'ImportPermission')->name('import.permission');
+
 });
 
 
